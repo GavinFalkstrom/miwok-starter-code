a9d3c10ab08d31560a28b3d7dd46722760e22646
GavinFalkstrom <gasvinfalkstrom@gmail.com>
2017-11-10 10:34:16 -0600

    Added activities

---
+++ familymembersactivity/src/main/java/com/example/android/familymembersactivity/familymembersactivity.java
@@ -0,0 +1,13 @@
+package com.example.android.familymembersactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class familymembersactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_familymembersactivity);
+    }
+}
